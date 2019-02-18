c164d1bb12a7ab6a7ab47cf1a4febd7730c85c30
Andrew Jarman <atjuegos@gmail.com>
2019-02-18 14:32:49 -0700

    Initial checkin of assignment 2.

---
+++ CSE360Assignment2/src/cse360assign2/CalculatorDriver.java
@@ -0,0 +1,19 @@
+package cse360assign2;
+
+public class CalculatorDriver
+{
+
+	public static void main(String[] args)
+	{
+		Calculator calculator = new Calculator();
+		
+		calculator.add(2);
+		calculator.add(4);
+		calculator.subtract(1);
+		calculator.multiply(2);
+		calculator.divide(3);
+		
+		System.out.print(calculator.getTotal());
+	}
+
+}
